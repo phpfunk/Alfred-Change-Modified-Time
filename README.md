c83e0462fabd1aebd7c51ecc832dc9a29a2e7d33
Jeff Johns <phpfunk@gmail.com>
2013-05-16 21:58:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+This extension will change the modification time of any file or all files within a directory the current date and time.
+
+This is helpful when using services like `git` where it preserves the modification time. Meaning you can pull files from a remote and the modification time will be the last time the other user changed the file.
+
+Anyway, maybe this will be helpful for someone :)
+
+### File Action
+This extension is a file action, meaning you look for files or a directory with files in Alfred. When you find it you can press the '>' key to access the file actions menu. Click 'Change Modified Time to Now' and the file or files will be updated.
+
+That's it.
+
+### Changelog
+
+**1.0** - May 16th, 2013
+* Initial commit
---
+++ script.php
@@ -0,0 +1,50 @@
+<?php
+
+//Function to get all files recursively from a directory
+function getFiles($dir, $files=array())
+{
+    $files = (empty($files)) ? array($dir) : $files;
+    if ($handle = opendir($dir)) {
+        while (false !== ($entry = readdir($handle))) {
+            $file = $dir . '/' . $entry;
+            if (substr($entry, 0, 1) != '.') {
+                if (is_dir($file)) {
+                   array_push($files, $file);
+                    $files = getFiles($file, $files);
+                }
+                else {
+                    array_push($files, $file);
+                }
+            }
+        }
+
+        closedir($handle);
+    }
+
+    return $files;
+}
+
+// Get the file or directory from Alfred
+// Set the plural to null
+$file = '{query}';
+$s    = null;
+
+// If is a directory
+// Get all the files
+// Set plural to 's'
+// Else just create the array
+if (is_dir($file)) {
+    $s     = 's';
+    $files = getFiles($file);
+}
+else {
+    $files = array($file);
+}
+
+// Loop through each file and update the modtime to now
+foreach ($files as $file) {
+    $s = shell_exec('touch –mt ' . '"' . $file . '"');
+}
+
+// Print message
+print 'File' . $s . ' updated to the current date.';
